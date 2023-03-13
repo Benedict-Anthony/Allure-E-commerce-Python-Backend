b8f74ee15e97e704612459b523b18c11efe6debf
Benedict Anthony <benedict.ughulu@gmail.com>
2023-03-13 06:58:10 -0700

    remove tutorial app and renamed to blog

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 
 from .models import Post
 
-admin.site.register(Post)
+from django.contrib import admin
+
+# from products.models import Category
+
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = ({"slug":("title",)})
+    
+admin.site.register(Post, PostAdmin)
+
+# admin.site.register(Asset)
