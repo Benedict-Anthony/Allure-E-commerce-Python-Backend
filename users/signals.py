5e46823a382076ce06cbc64c78ddd51d6a395bfe
Benedict Anthony <benedict.ughulu@gmail.com>
2023-05-29 16:43:34 -0700

    prep sign up email

--- users/signals.py
+++ users/signals.py
@@ -1,5 +1,5 @@
 from django.dispatch import receiver
-from django.db.models.signals import pre_save,post_save
+from django.db.models.signals import pre_save, post_save
 
 from users.token import user_token
 from .models import CustomUser
@@ -7,22 +7,18 @@ from .email import send_mail
 
 
 @receiver(post_save, sender=CustomUser)
-def pre_save_user(sender, instance, created,  **kwargs):
+def pre_save_user(sender, instance, created, **kwargs):
     if created:
-        token = user_token({"email":instance.email, "id":instance.id})
+        token = user_token({"email": instance.email, "id": instance.id})
         subject = "Allure Account Confirmation"
         email = instance.email
         body = f"""Hello {instance.first_name} {instance.last_name}, 
         Thanks for creating an account with allure\n\n
         To activate your account, please click on the link below:\n\n
-        http://localhost:3000/comfirm/{token}\n\n
+        https://allures.vercel.app//comfirm/{token}\n\n
         
         Allure
         
         """
-        
-        send_mail(email, subject, body)
-    
-    
-    
 
+        send_mail(email, subject, body)
