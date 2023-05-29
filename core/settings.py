a7dfb4d1fca8a36dba08fd61884497f0bff3235c
Benedict Anthony <benedict.ughulu@gmail.com>
2023-05-29 14:30:44 -0700

    prep for first deploy

--- core/settings.py
+++ core/settings.py
@@ -26,89 +26,80 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
     # Apps
     "blog.apps.BlogConfig",
     "products.apps.ProductsConfig",
     "users.apps.UsersConfig",
     "services.apps.ServicesConfig",
-    
     # Packeges
     "corsheaders",
     "rest_framework",
-    'rest_framework.authtoken',
-    'rest_framework_simplejwt',
-    'rest_framework_simplejwt.token_blacklist',
-    'drf_spectacular',
-
-    'cloudinary_storage',
-    'cloudinary',
-    
+    "rest_framework.authtoken",
+    "rest_framework_simplejwt",
+    "rest_framework_simplejwt.token_blacklist",
+    "drf_spectacular",
+    "cloudinary_storage",
+    "cloudinary",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'core.urls'
+ROOT_URLCONF = "core.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'social_django.context_processors.backends',
-                'social_django.context_processors.login_redirect',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "social_django.context_processors.backends",
+                "social_django.context_processors.login_redirect",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'core.wsgi.application'
+WSGI_APPLICATION = "core.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
-#     'default': dj_database_url.parse(config("DATABASE_URL"))
-        
-
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
-
+DATABASES = {"default": dj_database_url.parse(config("DATABASE_URL"))}
 
 
 # Password validation
@@ -116,64 +107,56 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
 
-
 REST_FRAMEWORK = {
-    
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
-    
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
     ),
-    
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-        ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100,
-    
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly",
+    ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 100,
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
 SPECTACULAR_SETTINGS = {
-    'TITLE': 'Allure API Docs',
-    'DESCRIPTION': 'API documentation for Allure',
-    'VERSION': '1.0.0',
-    'SERVE_INCLUDE_SCHEMA': False,
+    "TITLE": "Allure API Docs",
+    "DESCRIPTION": "API documentation for Allure",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
     # OTHER SETTINGS
 }
 
-AUTHENTICATION_BACKENDS = (
-    "django.contrib.auth.backends.ModelBackend",
-)
+AUTHENTICATION_BACKENDS = ("django.contrib.auth.backends.ModelBackend",)
 
 
-
-CORS_ALLOW_ALL_ORIGINS = True 
+CORS_ALLOW_ALL_ORIGINS = True
 
 ## CLOUDINARY SET UP
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': config("CLOUDINARY_NAME"),
-    'API_KEY': config("CLOUDINARY_KEY"),
-    'API_SECRET': config("CLOUDINARY_SECRET")
+    "CLOUD_NAME": config("CLOUDINARY_NAME"),
+    "API_KEY": config("CLOUDINARY_KEY"),
+    "API_SECRET": config("CLOUDINARY_SECRET"),
 }
 
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(days=10),
@@ -181,15 +164,15 @@ SIMPLE_JWT = {
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
-    }
+}
 AUTH_USER_MODEL = "users.CustomUser"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -201,9 +184,9 @@ MEDIA_URL = "/media/"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
--- products/models.py
+++ products/models.py
@@ -8,68 +8,76 @@ from django.core.files import File
 from users.models import CustomUser
 from publik.utils import custom_id
 
+
 class Category(models.Model):
     name = models.CharField(max_length=30, unique=True)
 
     def __str__(self):
         return self.name
 
+
 class ProductManager(models.Manager):
     def search(self, query):
         if query:
             return self.filter(Q(name__icontains=query) | Q(slug__icontains=query))
         return []
-    
-    
+
     def get_queryset(self):
         return super().get_queryset().filter(available=True)
-    
+
+
 class Products(models.Model):
-    id = models.CharField(max_length=30, default=custom_id, primary_key=True, editable=False, unique=True)
+    id = models.CharField(
+        max_length=30, default=custom_id, primary_key=True, editable=False, unique=True
+    )
     name = models.CharField(max_length=50)
     price = models.FloatField()
     discount = models.FloatField(blank=True, null=True)
     image = models.ImageField(upload_to="images", default="")
-    thumbnail = models.ImageField(upload_to="thumbnails", default="", blank=True, null=True)
+    thumbnail = models.ImageField(
+        upload_to="thumbnails", default="", blank=True, null=True
+    )
     percent = models.FloatField(null=True, blank=True)
     description = models.TextField(max_length=400)
-    size = models.CharField(max_length=15,  blank=True, null=True)
-    rating =models.IntegerField(default=1, blank=True, null=True)
-    category =  models.ForeignKey(Category, blank=True, on_delete=models.SET_NULL,null=True )
+    size = models.CharField(max_length=15, blank=True, null=True)
+    rating = models.IntegerField(default=1, blank=True, null=True)
+    category = models.ForeignKey(
+        Category, blank=True, on_delete=models.SET_NULL, null=True
+    )
     available = models.BooleanField(default=True)
     notify = models.BooleanField(default=False)
     notified = models.BooleanField(default=False)
     slug = models.SlugField(blank=True, null=True, default="", unique=True)
     created = models.DateTimeField(auto_now_add=True)
     update = models.DateTimeField(auto_now=True)
-    
+
     objects = models.Manager()
     available_products = ProductManager()
-    
+
     def __str__(self):
         return self.name
-    
+
     @property
     def image_url(self):
         try:
             return self.image.url
         except:
             return ""
-    
+
     @property
     def product_discount(self):
         if self.discount:
             return self.discount
         else:
             return 0.0
-        
+
     @property
     def product_price(self):
         if self.discount:
             return self.price - self.discount
-        
+
         return self.price
-    
+
     @property
     def disc_perc(self):
         if self.discount:
@@ -77,7 +85,6 @@ class Products(models.Model):
             self.save()
             return self.percent
         return 0
-    
 
     def thumbnail_url(self):
         if self.thumbnail:
@@ -87,9 +94,9 @@ class Products(models.Model):
                 self.thumbnail = self.make_thumbnail(image=self.image)
                 self.save()
                 return self.thumbnail.url
-            
+
         return ""
-    
+
     def make_thumbnail(self, image, size=(300, 200)):
         img = Image.open(image)
         img.convert("RGB")
@@ -99,46 +106,44 @@ class Products(models.Model):
 
         thumbnail = File(thumb_io, name=image.name)
         return thumbnail
-      
+
     class Meta:
         verbose_name_plural = "Products"
-        ordering =("-created",)
-   
-    
+        ordering = ("-created",)
 
 
 class OrderedItems(models.Model):
-    id = models.CharField(max_length=30, default=custom_id, primary_key=True, editable=False, unique=True)
+    id = models.CharField(
+        max_length=30, default=custom_id, primary_key=True, editable=False, unique=True
+    )
     customer = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True)
     product = models.ForeignKey(Products, on_delete=models.SET_NULL, null=True)
     quantity = models.IntegerField()
     price = models.IntegerField()
     transaction_id = models.CharField(max_length=100)
     date = models.DateField(auto_now=True)
-    
+
     class Meta:
         verbose_name_plural = "Ordered Item"
-
 
     def __str__(self):
         return self.product.name
 
+
 class Order(models.Model):
-    status = (
-        ("pending", "pending"),
-        ("sent", "sent"),
-        ("delivered", "delivered")
+    status = (("pending", "pending"), ("sent", "sent"), ("delivered", "delivered"))
+    id = models.CharField(
+        max_length=30, default=custom_id, primary_key=True, editable=False, unique=True
     )
-    id = models.CharField(max_length=30, default=custom_id, primary_key=True, editable=False, unique=True)
     customer = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    order = models.ForeignKey(OrderedItems, on_delete=models.CASCADE, related_name="orders")
+    order = models.ForeignKey(
+        OrderedItems, on_delete=models.CASCADE, related_name="orders"
+    )
     address = models.ForeignKey(Address, on_delete=models.SET_NULL, null=True)
     total_price = models.IntegerField()
     status = models.CharField(choices=status, default="pending", max_length=15)
     complete = models.BooleanField(default=False)
     date = models.DateField(auto_now=True)
-    
-    
-    
+
     class Meta:
         verbose_name_plural = "Orders"
